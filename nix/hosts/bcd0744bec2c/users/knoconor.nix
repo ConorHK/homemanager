53ab6040b751822a2bd8d12e5506050f4c68a2a5
Conor Knowles <knoconor@amazon.com>
2025-02-26 17:57:21 +0000

    Add laptop

---
+++ nix/hosts/bcd0744bec2c/users/knoconor.nix
@@ -0,0 +1,22 @@
+{
+  flake,
+  config,
+  lib,
+  ...
+}: with lib; {
+  imports = [
+    flake.homeModules.common-role
+    flake.homeModules.development-role
+  ];
+
+  config = {
+    cli.programs.git = {
+      defaultBranch = "mainline";
+      email = "knoconor@amazon.com";
+    };
+    home = {
+      stateVersion = "24.11";
+    };
+    cli.programs.ssh.enable = mkForce false;
+  };
+}
