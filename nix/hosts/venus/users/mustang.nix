4efe83300cedd4cddcf9deb1595b9a975711737e
Conor Knowles <>
2025-02-26 23:58:49 +0000

    Add server userfile

---
+++ nix/hosts/venus/users/mustang.nix
@@ -0,0 +1,28 @@
+{ flake, ... }:
+{
+  imports = [
+    flake.homeModules.common-role
+    flake.homeModules.development-role
+  ];
+
+  config = {
+    home = {
+      sessionVariables = {
+        BROWSER = "echo";
+      };
+      stateVersion = "24.11";
+    };
+    cli.programs.ssh = {
+      extraHosts.fuji = {
+        hostname = "fuji";
+        user = "conor";
+        port = 22;
+      };
+      extraHosts.dns = {
+        hostname = "dns";
+        user = "conor";
+        port = 22;
+      };
+    };
+  };
+}
