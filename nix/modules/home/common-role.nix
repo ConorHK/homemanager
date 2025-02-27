5889a2923243e1ab6e7e4f36abedff67495e53e1
Conor Knowles <knoconor@amazon.com>
2025-02-27 15:49:02 +0000

    Move from htop to btop

--- nix/modules/home/common-role.nix
+++ nix/modules/home/common-role.nix
@@ -37,12 +37,12 @@ with lib;
     shells.zsh.enable = true;
     programs = {
       bat.enable = true;
+      btop.enable = true;
       comma.enable = true;
       duf.enable = true;
       dust.enable = true;
       eza.enable = true;
       fzf.enable = true;
-      htop.enable = true;
       less.enable = true;
       nh.enable = true;
       ssh.enable = mkDefault true;
